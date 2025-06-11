a0245e2f7005749680470ef21bd8ea1b0c0d1522
mlemmmm <151975556+mlemmmm@users.noreply.github.com>
2025-06-11 17:42:26 -0400

    Update CONTAINS.js

--- helper/CONTAINS.js
+++ helper/CONTAINS.js
@@ -1,5 +1,5 @@
 /*
-desc: Boolean whether an array or range contains a specified string.
+desc: Boolean whether an array or range contains a specified value.
 var: range, value
 */
 =OR(MAP(range,LAMBDA(cell,cell=value)))

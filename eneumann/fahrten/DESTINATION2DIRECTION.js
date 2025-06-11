ba89e0464f415d00b9b52c2d436544db7533ce6b
mlemmmm <151975556+mlemmmm@users.noreply.github.com>
2025-06-11 16:40:16 +0200

    Create DESTINATION2DIRECTION.js

---
+++ eneumann/fahrten/DESTINATION2DIRECTION.js
@@ -0,0 +1,12 @@
+/*
+desc: Converts a string of destinations like "Ertingen - Bad Saulgau - Herbertingen - Ertingen" to a list of destinations like {"Ertingen~Bad Saulgau"; "Bad Saulgau~Herbertingen"; "Herbertingen~Ertingen"}               
+var: s
+s desc: A string of destinations
+s ex: "Ertingen - Bad Saulgau - Herbertingen - Ertingen"
+*/
+=BYROW(
+       LAMBDA(destinations,
+                     {ARRAY_CONSTRAIN(destinations,ROWS(destinations)-1,1),
+                      REVERSEROWS(ARRAY_CONSTRAIN(destinations,ROWS(destinations)-1,1))})
+       (TRANSPOSE(ARRAYFORMULA(TRIM(SPLIT(s,"-",TRUE,TRUE))))),
+       LAMBDA(row,JOIN("~",row)))

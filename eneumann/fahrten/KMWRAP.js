e55bc2dbbe19fda27df8f6216578d58209fb1611
mlemmmm <151975556+mlemmmm@users.noreply.github.com>
2025-06-11 19:22:39 -0400

    Update KMWRAP.js
    just before destination error catcher integration

--- eneumann/fahrten/KMWRAP.js
+++ eneumann/fahrten/KMWRAP.js
@@ -1,21 +1,26 @@
 =IF(LEN(B2)<4,, // catch unpopulated row in a robust manner, allowing for " - ", "- ", "-", empty, or any other short identifier of an unpopulated row
+
+ // catch destination errors
+ LAMBDA(destinations, 
+ 
  LAMBDA(kms,
-        IF(AND(ARRAYFORMULA(ISNUMBER(kms))), // catch all errors
+        IF(AND(ARRAYFORMULA(ISNUMBER(kms))), // catch direction errors
                SUM(kms), // Sum up km if there are no errors
-               JOIN(CHAR(10),TOCOL(MAP(kms,LAMBDA(km,IF(ISNUMBER(km),,km))),1)))) // Assemble all "unknown direction" errors into one error message
+               JOIN(CHAR(9888),CHAR(10),TOCOL(MAP(kms,LAMBDA(km,IF(ISNUMBER(km),,km))),1)))) // Assemble all "unknown direction" errors into one error message
  (MAP(
 
   // Convert destination string to direction list
-      BYROW(
-           LAMBDA(destinations, 
-                         {ARRAY_CONSTRAIN(destinations,ROWS(destinations)-1,1), // "from" list, cut off last row
-                          REVERSEROWS(ARRAY_CONSTRAIN(REVERSEROWS(destinations),ROWS(destinations)-1,1))}) // "to" list, cut off first row
+      BYROW({ARRAY_CONSTRAIN(destinations,ROWS(destinations)-1,1), // "from" list, cut off last row
+             REVERSEROWS(ARRAY_CONSTRAIN(REVERSEROWS(destinations),ROWS(destinations)-1,1))}, // "to" list, cut off first row
+             LAMBDA(row,JOIN("~",row))), // Add "~" between "from" and "to" lists, concatenate 
 
-            // Turn "from" and "to" into direction string
-           (TRANSPOSE(ARRAYFORMULA(TRIM(SPLIT(B2,"-",TRUE,TRUE))))), // Split string of destinations into list of destinations (first operation)
-            LAMBDA(row,JOIN("~",row))), // Add "~" between "from" and "to" lists, concatenate 
-
-  // Work through directions, output km for each direction
-  LAMBDA(direction,IF(CONTAINS(direction_db,direction), // catch "unknown direction" errors
+  // Work through directions, output km for each direction, catch direction errors
+  LAMBDA(direction,IF(CONTAINS(direction_db,direction), // query DB
                       FORCELOOKUP(direction,CHOOSECOLS(direction_db,1),CHOOSECOLS(direction_db,2)), // Look up km values for each direction
-                      CONCATENATE(CHAR(34),direction,CHAR(34)," fehlt")))))) // Assemble error message if direction unknown
+                      CONCATENATE(CHAR(9888),CHAR(34),direction,CHAR(34)," fehlt")))))) // Assemble error message if direction unknown
+
+    // Split string of destinations into list of destinations (first operation)
+    (TRANSPOSE(ARRAYFORMULA(TRIM(SPLIT(B2,"-",TRUE,TRUE))))) 
+
+
+

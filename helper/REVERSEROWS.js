da27ce2e5e30b16a73a8080ea97897b029cb51ff
mlemmmm <151975556+mlemmmm@users.noreply.github.com>
2025-06-11 14:49:43 +0200

    Update REVERSEROWS.js

--- helper/REVERSEROWS.js
+++ helper/REVERSEROWS.js
@@ -1,3 +1,9 @@
+/*
+desc: Reverses rows reliably no matter the content, even with empty rows
+var: range
+range desc: Any range of cells
+range ex: {1, 2; 3, 4}
+*/
 =ARRAY_CONSTRAIN(SORT(HSTACK(
                              range,
                              MAKEARRAY(ROWS(range),1,LAMBDA(row,col,row))),

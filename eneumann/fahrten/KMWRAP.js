036a552c3f047064213b71429ae32bcc4d969401
mlemmmm <151975556+mlemmmm@users.noreply.github.com>
2025-06-11 19:08:58 +0200

    Update KMWRAP.js
    collection

--- eneumann/fahrten/KMWRAP.js
+++ eneumann/fahrten/KMWRAP.js
@@ -2,3 +2,8 @@
  IF(NOT(RIGHT(str,8)="Ertingen"), "Error: Ankunftsort nicht Ertingen",
     
 ))
+
+
+=MAP(DEST2DIR(Fahrten!B3),LAMBDA(direction,
+BYROW(DB!F:G,LAMBDA(table_direction,IF(CHOOSECOLS(table_direction,1)=direction,CHOOSECOLS(table_direction,2),)
+))))

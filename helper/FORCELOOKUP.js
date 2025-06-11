77fd1a244915bb6ff69dc9e4abe5a3a668cff2d4
mlemmmm <151975556+mlemmmm@users.noreply.github.com>
2025-06-11 19:20:02 +0200

    Update FORCELOOKUP.js

--- helper/FORCELOOKUP.js
+++ helper/FORCELOOKUP.js
@@ -1,5 +1,5 @@
 /*
-desc: Forces a lookup on any unsorted range, higher computational complexity than LOOKUP
+desc: Forces a lookup on any unsorted range, higher computational complexity than LOOKUP. Returns all values, not just the first.
 var: search_key, search_range, result_range
 */
 =TOCOL(BYROW(HSTACK(search_range,result_range),LAMBDA(row,IF(CHOOSECOLS(row,1)=search_key,CHOOSECOLS(row,2),))),1)

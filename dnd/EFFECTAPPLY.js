1da05775e93d671965cfb14892e5839aff87d483
mlemmmm <151975556+mlemmmm@users.noreply.github.com>
2025-08-08 21:06:20 -0400

    Update EFFECTAPPLY.js

--- dnd/EFFECTAPPLY.js
+++ dnd/EFFECTAPPLY.js
@@ -4,6 +4,9 @@ var: effect, amount
 effect desc: The effect string of a module
 effect ex: "+50% Energy usage\n-10% Input usage"
 amount desc: how many of a module there are
+
+// TODO: 1st col aside, all other cols join with " ", then hstack. Use let(splitresult,...) to compact it  
+
 */
 
 =LET(effectarray, ARRAYFORMULA(SPLIT(SPREAD(effect)," ",FALSE)),

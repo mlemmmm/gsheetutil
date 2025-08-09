81544b43d53e7bee12a6b32c0f154b9be4ca2f80
mlemmmm <151975556+mlemmmm@users.noreply.github.com>
2025-08-09 17:31:13 -0400

    Update EFFECTFORMAT.js

--- dnd/EFFECTFORMAT.js
+++ dnd/EFFECTFORMAT.js
@@ -7,7 +7,7 @@ amount desc: how many of a module there are
 */
 
 =ARRAYFORMULA(ARRAYTRIM(IFERROR(LET(effectarray, ARRAYFORMULA(SPLIT(REGEXREPLACE(SPREAD(effect),"% ","%"),"%",FALSE)),
- LET(multiplier, MAP(CHOOSECOLS(effectarray,1),LAMBDA(percent,1+DROPLEFT(percent,1)/100)),
+ LET(multiplier, MAP(CHOOSECOLS(effectarray,1),LAMBDA(percent,IF(LEFT(percent,1)="+",1+DROPLEFT(percent,1)/100,1+percent/100))),
      description, CHOOSECOLS(effectarray,2),
      HSTACK(ARRAYFORMULA(multiplier^amount), description)
     )

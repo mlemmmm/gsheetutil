a3a83fc3cbb6232f50d96a15b98ae6da4eefbb1f
mlemmmm <151975556+mlemmmm@users.noreply.github.com>
2025-08-09 15:57:19 -0400

    Update EFFECTFORMAT.js

--- dnd/EFFECTFORMAT.js
+++ dnd/EFFECTFORMAT.js
@@ -4,9 +4,6 @@ var: effect, amount
 effect desc: The effect string of a module
 effect ex: "+50% Energy usage\n-10% Input usage"
 amount desc: how many of a module there are
-
-// TODO: 1st col aside, all other cols join with " ", then hstack. Use let(splitresult,...) to compact it  NO, USE REGEXREPLACE TO REPLACE "%\s"
-
 */
 
 =ARRAYFORMULA(ARRAYTRIM(IFERROR(LET(effectarray, ARRAYFORMULA(SPLIT(REGEXREPLACE(SPREAD(effect),"% ","%"),"%",FALSE)),

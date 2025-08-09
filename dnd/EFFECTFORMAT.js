9f0816ff239c8685e613388daf2a7112a7ec04af
mlemmmm <151975556+mlemmmm@users.noreply.github.com>
2025-08-09 14:55:58 -0400

    Update and rename EFFECTAPPLY.js to EFFECTFORMAT.js

--- dnd/EFFECTFORMAT.js
+++ dnd/EFFECTFORMAT.js
@@ -5,13 +5,13 @@ effect desc: The effect string of a module
 effect ex: "+50% Energy usage\n-10% Input usage"
 amount desc: how many of a module there are
 
-// TODO: 1st col aside, all other cols join with " ", then hstack. Use let(splitresult,...) to compact it  
+// TODO: 1st col aside, all other cols join with " ", then hstack. Use let(splitresult,...) to compact it  NO, USE REGEXREPLACE TO REPLACE "%\s"
 
 */
 
-=LET(effectarray, ARRAYFORMULA(SPLIT(SPREAD(effect)," ",FALSE)),
+=LET(effectarray, ARRAYFORMULA(SPLIT(REGEXREPLACE(SPREAD(effect),"% ","%"),"%",FALSE)),
  LET(multiplier, MAP(CHOOSECOLS(effectarray,1),LAMBDA(percent,1+DROPRIGHT(DROPLEFT(percent,1),1)/100)),
      description, CHOOSECOLS(effectarray,2),
-    
+     HSTACK(multiplier, description)
     )
 )
